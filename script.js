c367089a1209f685f9890b55fefc1d0dbcf63ff7
z9thegoat <naphat.nav@spumail.net>
2024-11-19 00:25:17 +0700

    update Homepage final mank

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 console.log("welcome wo Help");
 
+// Slide Navigation
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+// Next/Previous controls
 function plusSlides(n) {
   showSlides((slideIndex += n));
 }
@@ -32,3 +33,60 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+// Dropdown Menu Handling
+let dropdowns = document.querySelectorAll(".navbar .dropdown-toggler");
+let dropdownIsOpen = false;
+
+if (dropdowns.length) {
+  dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener("click", (event) => {
+      let target = document.querySelector(`#${event.target.dataset.dropdown}`);
+
+      if (target) {
+        if (target.classList.contains("show")) {
+          target.classList.remove("show");
+          dropdownIsOpen = false;
+        } else {
+          target.classList.add("show");
+          dropdownIsOpen = true;
+        }
+      }
+    });
+  });
+}
+
+// Handle closing dropdowns when clicking outside
+window.addEventListener("mouseup", (event) => {
+  if (dropdownIsOpen) {
+    dropdowns.forEach((dropdownButton) => {
+      let dropdown = document.querySelector(
+        `#${dropdownButton.dataset.dropdown}`
+      );
+      let targetIsDropdown = dropdown == event.target;
+
+      if (dropdownButton == event.target) {
+        return;
+      }
+
+      if (!targetIsDropdown && !dropdown.contains(event.target)) {
+        dropdown.classList.remove("show");
+      }
+    });
+  }
+});
+
+// Mobile Navigation Handling
+function handleSmallScreens() {
+  document.querySelector(".navbar-toggler").addEventListener("click", () => {
+    let navbarMenu = document.querySelector(".navbar-menu");
+
+    if (!navbarMenu.classList.contains("active")) {
+      navbarMenu.classList.add("active");
+    } else {
+      navbarMenu.classList.remove("active");
+    }
+  });
+}
+
+handleSmallScreens();
